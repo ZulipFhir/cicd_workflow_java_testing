6efe6090e4d79ff3a0f98f4dfc99651acd6b9b6e
ZulipFhir <70761759+ZulipFhir@users.noreply.github.com>
2023-01-24 10:57:37 +0530

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    @GetMapping("/test")
+    public String Test()
+    {
+        return "Working";
+    }
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+    }
+}
